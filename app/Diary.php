b5476ad1ef4a0b505a2a75ae4094938d5a4d7259
kriss <kriss12n@gmail.com>
2019-05-13 20:52:25 -0400

    tabla de agenda funcional

---
+++ app/Diary.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Diary extends Model
+{
+    protected $table = "Diary";
+    //protected $fillable = ["id","title_task","date_task","status_task","color"];
+}
